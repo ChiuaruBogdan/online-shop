9c75d3ab8b02268c09f4f18f309bbe5a50798196
Chiuaru Bogdan <bchiuaru94@gmail.com>
2019-09-11 19:02:05 +0300

    added YAML configuration example (properties de configurare)

--- src/main/java/org/fasttrackit/onlineshop/persistance/ProductRepository.java
+++ src/main/java/org/fasttrackit/onlineshop/persistance/ProductRepository.java
@@ -4,7 +4,7 @@ import org.fasttrackit.onlineshop.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-//cand o clasa primitiva are litera mare device Wrapper class exemplu  long - > Long
+//cand o clasa primitiva are litera mare devine Wrapper class exemplu  long - > Long
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
